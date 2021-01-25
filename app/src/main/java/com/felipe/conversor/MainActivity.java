6329a007ac23b1b47d75f23a2296955b097c4910
FelipeOliver00 <Felipecastro0208@hotmail.com>
2021-01-24 23:12:43 -0300

    finished project

--- app/src/main/java/com/felipe/conversor/MainActivity.java
+++ app/src/main/java/com/felipe/conversor/MainActivity.java
@@ -3,12 +3,61 @@ package com.felipe.conversor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    //ViewHolder é uma pratica para agrupar um conjunto de views e reutilizar
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    //Méotod que criar a activity e inicializa os elementos de interface
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+
+        this.clearValues();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_calculate){
+            String value = this.mViewHolder.editValue.getText().toString();
+            if("".equals(value)){
+                //Mostra msg pro usuário
+                Toast.makeText(this,this.getString( R.string.informe_valor), Toast.LENGTH_LONG).show();
+            }else{
+                Double real = Double.valueOf(value);
+
+                this.mViewHolder.textDolar.setText((String.format("%.2f",(real/4))));
+                this.mViewHolder.textEuro.setText((String.format("%.2f",(real/5))));
+            }
+        }
+    }
+
+    private void clearValues (){
+        this.mViewHolder.textDolar.setText("");
+        this.mViewHolder.textEuro.setText("");
+    }
+
+
+    private static class ViewHolder{
+        EditText editValue;
+        TextView textDolar;
+        TextView textEuro;
+        Button buttonCalculate;
     }
 }
